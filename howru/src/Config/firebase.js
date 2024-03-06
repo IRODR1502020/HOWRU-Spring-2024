fd533f4e1f4e10fcb3abf6aa4f41b0e1ccc71abb
IRODR1502020 <irodr150@fiu.edu>
2024-03-06 13:09:17 -0500

    add firebase skeleton & ignore .env

---
+++ howru/src/Config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

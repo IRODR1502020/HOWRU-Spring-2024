393a0a655cb155814f75145d25eb7df12b6dd073
IRODR1502020 <irodr150@fiu.edu>
2024-03-07 20:26:06 -0500

    upload logo, wire up password reset
    do not push firebase creds (& firestore not initialized). uploaded HOWRU logo. password reset is mostly done

--- howru/src/Components/LoginWidget.js
+++ howru/src/Components/LoginWidget.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { Container, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import '../Styling/bootstrap.css';
 
-export default function LoginWidget () {
+const LoginWidget = () => {
 	const navigate = useNavigate();
 	
     return (
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"/>
-            </div>
-            <button type="submit" class="btn btn-success">Login</button>
-            <button type="submit" class="btn btn-primary">Sign Up</button>
-            <button type="button" onClick={() => navigate('/forgot-password')} class="btn btn-secondary">Forgot Password</button>
-        </form>
-    )
+		<Container>
+			<form>
+				<div class="mb-3">
+					<label for="exampleInputEmail1" class="form-label">Email address</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+					<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+				</div>
+				<div class="mb-3">
+					<label for="exampleInputPassword1" class="form-label">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword1"/>
+				</div>
+				<button type="submit" class="btn btn-success">Login</button>
+				<button type="submit" class="btn btn-primary">Sign Up</button>
+				<button type="button" onClick={() => navigate('/forgot-password')} class="btn btn-secondary">Forgot Password</button>
+			</form>
+			<Button variant="contained" onClick={() => navigate('/')} style={{ marginTop: '2rem' }}>
+				Return to Home
+			</Button>
+		</Container>
+		
+    );
 }
+
+export default LoginWidget;
--- howru/src/Components/ResetPassword.js
+++ howru/src/Components/ResetPassword.js
@@ -1,10 +1,79 @@
 import React, { useState } from "react";
+import { TextField, Button, Box, Typography, Link } from '@mui/material';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../Config/firebase.js';
 import { useNavigate } from 'react-router-dom';
 import { Link as RouterLink } from "react-router-dom";
+import logo from "../Assets/logo.png"
 
 const ResetPassword = () => {
+	const [email, setEmail] = useState('');
+	const [isEmailSent, setIsEmailSent] = useState(false);
+	const navigate = useNavigate();
 	
+	const handleResetPassword = async () => {
+		try {
+			await sendPasswordResetEmail(auth, email);
+			setIsEmailSent(true);
+		} catch (error) {
+			console.error("Error sending reset email! Error code: ", error);
+		}
+	};
 	
+	const logoContainerStyle = {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: "2rem",
+	};
+
+	const logoStyle = {
+		height: "350px",
+		marginBottom: "-2.5rem",
+		marginTop: "-2rem",
+		width: "auto",
+	 };
+	
+	return (
+		<Box
+		  sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: '100vh',
+		  }}
+		>
+		  <Box sx={{ width: '300px', mb: 4 }}>
+		  <div style={logoContainerStyle}>
+			<Link component={RouterLink} to="/login" underline="none">
+			  <img src={logo} alt="logo" style={logoStyle} />
+			</Link>
+		  </div>
+			{isEmailSent ? (
+			  <Typography variant="body1" align="center" mb={2}>
+				Password reset email sent. Please check your inbox for instructions!
+			  </Typography>
+			) : null}
+			<TextField
+			  label="Email"
+			  type="email"
+			  value={email}
+			  onChange={(e) => setEmail(e.target.value)}
+			  placeholder="Enter your email"
+			  fullWidth
+			  margin="normal"
+			  size="small"
+			/>
+			<Button variant="contained" onClick={handleResetPassword} fullWidth>
+			  Reset Password
+			</Button>
+			<Button variant="contained" onClick={() => navigate('/login')} fullWidth style={{ marginTop: '2rem' }}>
+			  Return to Login
+			</Button>
+		  </Box>
+		</Box>
+	);
 	
 }
 

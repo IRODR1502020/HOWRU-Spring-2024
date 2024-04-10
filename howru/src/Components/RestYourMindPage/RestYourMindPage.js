c2e3dfa793b41b0bfed4abf420215f2664369216
IRODR1502020 <irodr150@fiu.edu>
2024-04-10 15:00:30 -0400

    add popover to RestMyMind page
    created a popover effect

--- howru/src/Components/RestYourMindPage/RestYourMindPage.js
+++ howru/src/Components/RestYourMindPage/RestYourMindPage.js
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Popover } from "@mui/material"
 import SkeletonForAllPages from "../GeneralComponents/SkeletonForAllPages";
 import "./RestYourMindPageStyling/RestYourMindPage.scss";
 
 const RestYourMindPage = () => {
+	const [destressPopover, setDestressPopover] = useState(null);
+	const [sleepPopover, setSleepPopover] = useState(false);
+	const [focusPopover, setFocusPopover] = useState(false);
+	
+	const handleDestressClick = (event) => {
+		setDestressPopover(event.currentTarget);
+	};
+  
+	const handleClose = () => {
+		setDestressPopover(null);
+		setSleepPopover(null);
+		setFocusPopover(null);
+	}
+	
+	const openDestress = Boolean(destressPopover);
 	
 	return (
 	<SkeletonForAllPages>
-		<div className="exercisecontainer"> 
-			<h1> 
-				Meditation Techniques
-			</h1>
-			<h3>
-				I want to...
-			</h3>
-			<button type="button" className="exercisecontainer exbutton"> 
-				Destress my day! 
-			</button>
-			<button type="button" className="exercisecontainer exbutton"> 
-				Ease my sleep! 
-			</button>
-			<button type="button" className="exercisecontainer exbutton"> 
-				Strengthen my focus!
-			</button>
-			
-		
+		<div className="container-wrapper">
+			<div className="exercisecontainer"> 
+				<h1> 
+					Meditation Techniques
+				</h1>
+				<h3>
+					I want to...
+				</h3>
+				<button aria-describedby="button1" type="button" className="exercisecontainer exbutton" onClick={handleDestressClick}> 
+					Destress my day! 
+				</button>
+				{destressPopover && (
+						<Popover
+						id="button1"
+						anchorEl={destressPopover}
+						open={openDestress}
+						onClose={handleClose}
+						anchorOrigin={{
+						  vertical: 'bottom',
+						  horizontal: 'left',
+						}}> 
+							I'm a popover! 
+						</Popover>
+				)}
+				<button type="button" className="exercisecontainer exbutton"> 
+					Ease my sleep! 
+				</button>
+				<button type="button" className="exercisecontainer exbutton"> 
+					Strengthen my focus!
+				</button>
+
+			</div>
 		</div>
 	</SkeletonForAllPages>
 	);

4fbd70137e292b30624a243590a8f8b0513271a7
Rafael Ojeda <rafael.e.ojeda@gmail.com>
2024-04-10 10:11:13 -0400

    Logout button moved to AccountNavbar and appears if user is logged in or not

--- howru/src/Components/GeneralComponents/AccountNavbar.js
+++ howru/src/Components/GeneralComponents/AccountNavbar.js
@@ -4,8 +4,12 @@ import { Button, Typography, Link } from "@mui/material";
 import Bootstrap from 'react-bootstrap';
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
+import AuthServiceInstance from "./AuthService";
+import { create } from "@mui/material/styles/createTransitions";
 
 const AccountNavbar = () => {
+    const navigate = useNavigate();
+
 	const buttonPurpleBackground = {
 		backgroundImage : "linear-gradient(135deg, rgb(83, 64, 203) 0%, rgb(137, 70, 163) 100%)",
 		color: "#FFF",
@@ -19,6 +23,19 @@ const AccountNavbar = () => {
 		boxShadow: "5px 5px 5px 0px rgba(0,0,0,0.75)"
 	}
 
+	const handleLogout = () => {
+		const userEmail = {
+			'email': sessionStorage.getItem("user_email")
+		}
+		
+		AuthServiceInstance.logoutUser(userEmail);
+		//console.log(response)
+		
+		sessionStorage.clear();
+		navigate("/login");
+		
+	}
+
     return (
         <div className="row">
             <div className="col-md-12">
@@ -26,7 +43,9 @@ const AccountNavbar = () => {
                     <Typography variant="body2" align="right">
                         {
                             sessionStorage.getItem('name') === null ?
-                                null : (<h4 className="d-inline-block pr-2 m-0">Hello, {sessionStorage.getItem('name')}</h4>)
+                                null : (<span><h4 className="d-inline-block pr-2 m-0">Hello, {sessionStorage.getItem('name')}</h4> 
+                                        <Link style={ createAccountButton } type="button" onClick={handleLogout} underline="hover" className="p-2 m-2"> Logout </Link></span>)
+
                         }
 						{
                             sessionStorage.getItem('name') !== null ?
